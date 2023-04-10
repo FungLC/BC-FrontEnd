6c569129ec6553c18065a363d3c9c44f99297f58
V1NW0G <vincentwongck110@gmail.com>
2023-04-10 15:44:23 +0800

    frontend update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,40 @@ import { useState, useEffect } from "react";
 import lightabi from "./utils/LightControl.json";
 import payabi from "./utils/PaymentContract.json";
 import hotelabi from "./utils/Hotel.json";
+// mui
+import Button from '@mui/material/Button';
+import LogoutIcon from '@mui/icons-material/Logout';
+import WalletIcon from '@mui/icons-material/Wallet';
+import IconButton from '@mui/material/IconButton';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import SensorDoorOutlinedIcon from '@mui/icons-material/SensorDoorOutlined';
+import ContentPasteSearchOutlinedIcon from '@mui/icons-material/ContentPasteSearchOutlined';
+import RequestQuoteOutlinedIcon from '@mui/icons-material/RequestQuoteOutlined';
+import SwitchAccessShortcutAddOutlinedIcon from '@mui/icons-material/SwitchAccessShortcutAddOutlined';
+import SmokeFreeOutlinedIcon from '@mui/icons-material/SmokeFreeOutlined';
+import BedOutlinedIcon from '@mui/icons-material/BedOutlined';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+import EventAvailableOutlinedIcon from '@mui/icons-material/EventAvailableOutlined';
+import backgroundImage from './image/hotel.jpg';
+import vip from './image/vip.jpg';
+import metamaskgif from './image/metamask.gif'
+import { color, width } from '@mui/system';
+import CircleOutlined from '@mui/icons-material/CircleOutlined';
+import { Opacity } from '@mui/icons-material';
+
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+import Typography from '@mui/material/Typography';
+
+
 const { ethers } = require("ethers");
 
 const LightcontractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
@@ -20,9 +54,11 @@ const hotelABI = hotelabi.abi
 function App() {
   const [isOn, setlight] = useState(false)
   const [account, setAccount] = useState(null);
-  const [checkin, setcCheckin] = useState(false);
+  // const [checkin, setcCheckin] = useState(false);
+  const [checkin, setcCheckin] = useState(true);
   const [messages, setMessages] = useState([]);
   const [VIPcheck, setVip] = useState(false);
+  const [showPass,setShowPass] = useState(false);
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -168,6 +204,8 @@ function App() {
     }
   }
 
+ 
+
   const checkIn = async () => {
     try {
       const { ethereum } = window;
@@ -220,76 +258,121 @@ function App() {
   function toggle() {
     var button = document.getElementById("toggleButton");
     var details = document.getElementById("details");
-
+    setShowPass(!showPass);
     if (details.innerHTML === `<h4 align="center">Check In as ***********</h4>`) {
       details.innerHTML = `<h4 align="center">Check In as ${account.substring(0, 4)}...${account.substring(account.length - 4)}</h4>`;
       button.innerHTML = "Show Off";
     } else {
-      details.innerHTML = `<h4 align="center">Check In as ***********</h4>`;
+      details.innerHTML = `<h4 align="center">Auth with ***********</h4>`;
       button.innerHTML = "Show On";
     }
   }
 
 
   return (
-    <div className="container">
+    
+    <div className="container" >
+    
       {!checkin ? (
         <>
           <div className="centerup">
-            <h1>Welcome to Big God Hotel</h1>
+            <h1>Welcome to B.C. Hotel</h1>
             <br></br>
             <div className="ButtonM">
               <button className="button1" onClick={handleCheckIn}>
                 Check In
               </button>
+              
             </div>
           </div>
         </>
       ) : (
         <>
-          <h1>Welcome to Big God Hotel</h1>
-          <h2 align="center">Welcome, Mr.Big God</h2>
+        
+        <div className="bg-fixed" style={{ backgroundImage: `url(${backgroundImage})`,backgroundRepeat: "no-repeat", width:"100%", height:250}}>
+  
+        {/* <img className="bg-fixed" src={backgroundImage} alt="Hotel image" style={{width:"100%"}} />  */}
+        {/* style={{ backgroundImage: `url(${backgroundImage})`,backgroundRepeat: "no-repeat", width:"100%"}} */}
+        <div class="headline-container"><h1 class="headline headline--themovement headline--large">Exprience Blockchain Hotel</h1></div>
+          
+        
+        </div>
+
+        <div>
+          <br/>
+    <Card sx={{ maxWidth: 358,height:300 }}>
+      <CardMedia
+        sx={{ height: 170 }}
+        image={metamaskgif}
+        title="green iguana"
+      />
+      <CardContent>
+      <h2 align="center">Welcome, Mr. Cheng</h2>
           <div id="container">
-            <h4><p id="details">Check In as ***********</p></h4>
+            <h4 align="center"><p id="details">Auth with ***********</p></h4>
             <>&nbsp;</>
-            <button id="toggleButton" onClick={toggle}>Show On</button>
-          </div>
-          <div className="ButtonM">
-            <button className="button4" onClick={handleCheckOut}>
-              CheckOut
-            </button>
-          </div>
+  
+            <IconButton aria-label="fingerprint" onClick={toggle}>
+              {showPass ? <VisibilityIcon /> : <VisibilityOffIcon/>}
+              
+            </IconButton></div>
+      </CardContent>
+      
+    </Card>
+  </div>
           <br></br>
           <div className="row">
             <div className="col">
               <div className="tabs">
-                <div className="tab">
+                <div className="tab text-center">
                   <input type="checkbox" id="chck1"></input>
-                  <label className="tab-label" htmlFor="chck1">Light</label>
+                  <label className="tab-label" htmlFor="chck1"><SensorDoorOutlinedIcon/>Smart Door</label>
                   <div className="tab-content">
+                  
+                    
+                  
                     {!isOn ? (
-                      <>The light should be <b>Off</b> now, Click it to On<br></br><br></br>
-                        <button className="button4" onClick={light}>On</button>
+                      <>The door should be <b style={{ color: 'red' }}>Lock</b> now <br/><br/> Click it to Unlock<br></br>
+    
+                        <IconButton aria-label="fingerprint" color="error" onClick={light}>
+                          <LockOutlinedIcon sx={{ fontSize: 80 }}/>
+                        </IconButton> 
                       </>
                     ) : (
-                      <>The light should be <b>On</b> now, Click it to Off<br></br><br></br>
-                        <button className="button5" onClick={light}>Off</button></>
+                      
+                      <>The door should be <b style={{ color: 'green' }}>UnLock</b> now <br/> Click it to Lock<br></br><br></br>
+                        
+                        <IconButton aria-label="fingerprint" color="success" onClick={light}>
+                          <LockOpenOutlinedIcon sx={{ fontSize: 80 }}/>
+                        </IconButton> 
+                        </>
 
                     )}
                   </div>
                 </div>
-                <div className="tab">
+                <div className="tab text-center" >
                   <input type="checkbox" id="chck2"></input>
-                  <label className="tab-label" htmlFor="chck2">Check In Details</label>
+                  <label className="tab-label" htmlFor="chck2"><ContentPasteSearchOutlinedIcon/>Check In Details</label>
                   <div className="tab-content">
-                    You have paid : 7 ETH (1 ETH per day)for Check In
-                    <br></br>
-                    Now you had Check in 5 days 
+                    <b>Mr. Cheng</b><br/>
+                    Room:1701<br/><br/>
+                    <p>Check in  date: 10 Apr,2023</p>
+                    <p>Check out date: 17 Apr,2023</p>
+                    <p><SmokeFreeOutlinedIcon/> Smoke Free Room</p>
+                    <p><BedOutlinedIcon/> Double bed</p>
+                    Room fee: 1 ETH / day<br/>
+                    -----------------------------------<br/>
+                    Deposit : 7 ETH<br/><br/>
+                    
+                    
+                    <CheckCircleIcon sx={{ fontSize: 35}} color="success"/><CheckCircleIcon sx={{ fontSize: 35}} color="success"/><CheckCircleIcon sx={{ fontSize: 35}} color="success"/><CheckCircleIcon sx={{ fontSize: 35}} color="success"/><CheckCircleIcon sx={{ fontSize: 35}} color="success"/><CircleOutlined sx={{ fontSize: 35}}/><CircleOutlined sx={{ fontSize: 35}}/>
+                    Remaining <b>2</b> days  <br/>
+                    
                   </div>
                 </div>
-                <div className="tab">
+                <div className="tab text-center">
                       <input type="checkbox" id="chck3"></input>
-                      <label className="tab-label" htmlFor="chck3">Fees</label>
+                      <label className="tab-label" htmlFor="chck3"><RequestQuoteOutlinedIcon/>Extra Fees</label>
                       <div className="tab-content">
                         Coke 1 x 0.1 ETH<br></br>
                         Pizza 1 x 0.3 ETH<br></br>
@@ -298,21 +381,22 @@ function App() {
                         ---------------<br></br>
                         Total <b>1 ETH</b><br></br><br></br>
 
-                        <button className="button4" onClick={pay}>
-                          Pay
-                        </button>
+                        <button class="button-17" role="button" onClick={pay}>Payment  </button>
                       </div>
                     </div>
-                    <div className="tab">
+                
+                    <div className="tab text-center">
                       <input type="checkbox" id="chck4"></input>
-                      <label className="tab-label" htmlFor="chck4">VIP</label>
+                      <label className="tab-label" htmlFor="chck4"><SwitchAccessShortcutAddOutlinedIcon/>VIP Service</label>
                       <div className="tab-content">
                         {!VIPcheck? (
                           <>
-                          <>10 ETH for <b>VIP</b> service</>
+                          <img src={vip} alt="VIP image" />
+                          <>Just 10 ETH to enjoy <b>VIP</b> service</>
                           <br></br>
                           <br></br>
-                        <button className="button4" onClick={VIP}>Request for VIP</button>
+                
+                        <button class="button-85" role="button" onClick={VIP}>Join VIP</button>
                         </>
                         ):(
                           <>
@@ -321,20 +405,71 @@ function App() {
                         style={{ height: "auto", maxWidth: "100%", width: "100%", size:256}}
                         value={JSON.stringify(messages)} />
                       </div>
+                      
                       </>
+                      
                       )}
                       </div>
+                      
                     </div>
+
+
+                    <div className="tab text-center">
+                      <input type="checkbox" id="chck5"></input>
+                      <label className="tab-label" htmlFor="chck5"><LogoutIcon/>Check out</label>
+                      <div className="tab-content">
+                          Are you sure to Check out ?<br/><br/>    
+                          <button class="button-17" role="button" onClick={handleCheckOut}>Logout <LogoutIcon fontSize="small"/></button>
+                      </div>
+                    </div>
+
+                    
               </div>
+              <div className='text-center p-4' style={{ backgroundColor: '#ecf0f1'}}>
+                      © 2023 Copyright:<br/>
+                      <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+                        Powered by Blockchain Hotel System 
+                      </a>
+                </div>
             </div>
+           
           </div>
         </>
       )}
       <div>
       </div>
+      <div className="selection-box">
+  <div className="row">
+    <div className="col">
+      <div className="tab">
+        ...
+      </div>
     </div>
+    <div className="col">
+      <div className="tab">
+        ...
+      </div>
+    </div>
+  </div>
+  <div className="row">
+    <div className="col">
+      <div className="tab">
+        ...
+      </div>
+    </div>
+    <div className="col">
+      <div className="tab">
+        ...
+      </div>
+    </div>
+  </div>
+</div>
+
+{/* <div className='container'><button class="button-53" role="button">Button 53</button></div> */}
+</div>
   );
 }
 
 export default App;
+
 
